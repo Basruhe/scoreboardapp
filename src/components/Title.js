c1f363f989235e50b999d5bf77b5f2f7a7001989
Basruhe <sb.ruhe@gmail.com>
2020-08-19 14:23:18 +0200

    added component for content

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Title extends Component {
   render() {
-    return <h1>{this.props.content}</h1>;
+    return (
+      <div>
+        <h1>{this.props.content}</h1>
+        <h2>test</h2>
+      </div>
+    );
   }
 }
